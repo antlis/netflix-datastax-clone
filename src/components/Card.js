0ce993f03feb7e9ed668aebc7e66d515755b3b02
antlis <antlis@protonmail.com>
2021-07-04 22:38:55 +0500

    Initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const Card = ({ movie }) => {
+  const [isShown, setIsShown] = useState(false);
+  const { title, duration, thumbnail } = movie;
+
+  return (
+    <div
+      className="card"
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+    >
+      {!isShown && (
+        <video className="video">
+          <source src={thumbnail} type="video/mp4" />
+        </video>
+      )}
+
+      {isShown && (
+        <>
+          <video className="video" autoPlay={true} loop>
+            <source src={thumbnail} type="video/mp4" />
+          </video>
+          <div className="info-box">
+            <p>{movie.title}</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Card;
